dae5d2468ee7d72146740258bfec01bd020a0aba
nidorx <nidorx@nidorx-work-mint>
2013-09-09 21:02:50 -0300

    Melhoria de codigo e documentação

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 module.exports = (function() {
-    var EventEmitter = require('events').EventEmitter;
     var fs = require('fs');
     var Step = require('step');
     var Contextify = require('contextify');
@@ -11,221 +10,248 @@ module.exports = (function() {
         };
     }
 
-    var CheerioTemplate = new EventEmitter();
+    var CheerioTemplate = {
+        engine: function(filename, options, callback) {
+            /**
+             * Loads view script
+             * @param {string} path Path of view
+             * @param {function} callback
+             */
+            var loadView = function(path, callback) {
+                var view = {
+                    path: path
+                };
+                fs.stat(path, function(err, result) {
+                    if (err) {
+                        return callback(err);
+                    }
+                    fs.readFile(path, function(err, str) {
+                        if (err) {
+                            return callback(err, view);
+                        }
 
-    CheerioTemplate.engine = function(filename, options, callback) {
-        /**
-         * Carrega o arquivo
-         */
-        var loadView = function(path, callback) {
-            var view = {
-                path: path
+
+                        view.$ = cheerio.load(str);
+                        view.blocks = {};
+                        view.extend = null;
+                        view.sandbox = {
+                            mixin: {},
+                            console: console,
+                            data: options,
+                            $: view.$
+                        };
+                        var window = Contextify(view.sandbox);
+                        view.sandbox.window = window.getGlobal();
+                        view.sandbox.window.$ = view.$;
+                        return callback(null, view);
+                    });
+                });
             };
-            fs.stat(path, function(err, result) {
+
+            /**
+             * Load and run script
+             *
+             * @param {mixed} err
+             * @param {object} view
+             * @returns {null}
+             */
+            loadView(filename, function(err, view) {
                 if (err) {
                     return callback(err);
                 }
-                fs.readFile(path, function(err, str) {
-                    if (err) {
-                        return callback(err, view);
-                    }
 
-
-                    view.$ = cheerio.load(str);
-                    view.mtime = result.mtime;
-                    view.blocks = {};
-                    view.extend = null;
-                    view.sandbox = {
-                        mixin: {},
-                        console: console,
-                        data: options,
-                        $: view.$
-                    };
-                    var window = Contextify(view.sandbox);
-                    view.sandbox.window = window.getGlobal();
-                    view.sandbox.window.$ = view.$;
-                    return callback(null, view);
-                });
-            });
-        };
-
-        loadView(filename, function(err, view) {
-            if (err) {
-                return callback(err);
-            }
-
-            var stepParseExtends = function() {
-                //Verifica se extend
-                var group = this.group();
-
-                view.$('script[type="cheerio-template-extends"]').each(function() {
-                    var script = this;
-                    var done = group();
-                    if (script.attr('extends') === undefined) {
-                        return done();
-                    }
-                    var parentName = script.attr('extends');
-                    var layout = options.settings.views + '/' + parentName + '.html';
-                    view.extend = layout;
-                    script.remove();
-                    return done();
-                });
-            };
-
-
-            var stepParseBlocks = function() {
-                //Inclusao de blocos filhos
-                var group = this.group();
-
-                view.$('script[type="cheerio-template-block"]').each(function() {
-                    var script = this;
-                    var done = group();
-                    if (script.attr('block') === undefined) {
-                        return done();
-                    }
-                    var blockName = script.attr('block');
-                    if (options.__child) {
-                        script.before(options.__child.$('[cheerio-template-block="' + blockName + '"]').html());
-                    }
-                    script.remove();
-                    return done();
-                });
-            };
-
-            var stepParseIncludes = function() {
-                //Includes
-                var group = this.group();
-
-                //Ao extender, nao executa o script no filho, apenas no pai
-                if (view.extend) {
-                    return;
-                }
-
-                view.$('script[type="cheerio-template-include"]').each(function() {
-                    var scriptInclude = this;
-                    var done = group();
-                    if (scriptInclude.attr('include') === undefined) {
-                        return done();
-                    }
-                    var includeName = scriptInclude.attr('include');
-                    var path = options.settings.views + '/' + includeName + '.html';
-                    fs.stat(path, function(err, result) {
-                        if (err) {
-                            return callback(err);
+                /**
+                 * Parse extends, if exists
+                 */
+                var stepParseExtends = function() {
+                    var group = this.group();
+                    var count = 0;
+                    view.$('script[type="cheerio-template-extends"]').eq(0).each(function() {
+                        var done = group();
+                        if (this.attr('extends') === undefined) {
+                            this.remove();//Remove script tag
+                            return done();
                         }
-                        fs.readFile(path, function(err, str) {
+
+                        //parse only first extends tag, remove all others
+                        if (count === 0) {
+                            var parentName = this.attr('extends');
+                            var parentLayout = options.settings.views + '/' + parentName + '.html';
+                            view.extend = parentLayout;
+                            count++;
+                        }
+
+                        this.remove();//Remove script tag
+                        return done();
+                    });
+                };
+
+                /**
+                 * Parse all script blocks, if exists
+                 */
+                var stepParseBlocks = function() {
+                    var group = this.group();
+
+                    view.$('script[type="cheerio-template-block"]').each(function() {
+                        var done = group();
+                        if (this.attr('block') === undefined) {
+                            return done();
+                        }
+                        var blockName = this.attr('block');
+
+                        //If this script has child
+                        if (options.__child) {
+                            this.before(options.__child.$('[cheerio-template-block="' + blockName + '"]').html());
+                        }
+                        this.remove();
+                        return done();
+                    });
+                };
+
+                /**
+                 * Parse includes, if exists
+                 */
+                var stepParseIncludes = function() {
+                    var group = this.group();
+
+                    //Skip, if it extends other
+                    //childs content block was inserted in parent code after this step
+                    if (view.extend) {
+                        return;
+                    }
+
+                    view.$('script[type="cheerio-template-include"]').each(function() {
+                        var scriptInclude = this;
+                        var done = group();
+                        if (scriptInclude.attr('include') === undefined) {
+                            return done();
+                        }
+                        var includeName = scriptInclude.attr('include');
+                        var path = options.settings.views + '/' + includeName + '.html';
+                        fs.stat(path, function(err, result) {
                             if (err) {
                                 return callback(err);
                             }
-                            scriptInclude.before(str + '');
-                            scriptInclude.remove();
-                            return done();
+                            fs.readFile(path, function(err, str) {
+                                if (err) {
+                                    return callback(err);
+                                }
+                                scriptInclude.before(str + '');
+                                scriptInclude.remove();
+                                return done();
+                            });
                         });
                     });
-                });
-            };
+                };
 
-            var stepParseMixins = function() {
-                //Mixins
-                var group = this.group();
+                /**
+                 * Parse all mixins
+                 */
+                var stepParseMixins = function() {
+                    var group = this.group();
 
-                //Ao extender, nao executa o script no filho, apenas no pai
-                if (view.extend) {
-                    return;
-                }
-                view.$('[cheerio-template-mixin]').each(function() {
-                    var done = group();
-                    var $ = cheerio.load(this.html());
-                    var mixinName = this.attr('cheerio-template-mixin');
+                    //Skip, if it extends other
+                    //childs content block was inserted in parent code after this step
+                    if (view.extend) {
+                        return;
+                    }
 
-                    //
-                    if (mixinName === undefined) {
+                    view.$('[cheerio-template-mixin]').each(function() {
+                        var done = group();
+                        var $ = cheerio.load(this.html());
+                        var mixinName = this.attr('cheerio-template-mixin');
+
+                        //
+                        if (mixinName === undefined) {
+                            return done();
+                        }
+
+                        mixinSandbox = {
+                            console: console,
+                            data: options,
+                            $: $
+                        };
+                        var mixinWindow = Contextify(mixinSandbox);
+                        mixinSandbox.window = mixinWindow.getGlobal();
+                        mixinSandbox.window.$ = mixinSandbox.$;
+
+
+
+                        var source = this.find('script[mixin="' + mixinName + '"]').html().trim();
+                        if (source === '') {
+                            return done();
+                        }
+                        mixinSandbox.run(source);
+                        view.sandbox.mixin[mixinName] = function() {
+                            return mixinSandbox[mixinName].apply(this, arguments);
+                        };
+
+                        this.remove();
                         return done();
-                    }
-
-                    mixinSandbox = {
-                        console: console,
-                        data: options,
-                        $: $
-                    };
-                    var mixinWindow = Contextify(mixinSandbox);
-                    mixinSandbox.window = mixinWindow.getGlobal();
-                    mixinSandbox.window.$ = mixinSandbox.$;
-
-
-
-                    var source = this.find('script[mixin="' + mixinName + '"]').html().trim();
-                    if (source === '') {
-                        return done();
-                    }
-                    mixinSandbox.run(source);
-                    view.sandbox.mixin[mixinName] = function() {
-                        return mixinSandbox[mixinName].apply(this, arguments);
-                    };
-
-                    this.remove();
-                    return done();
-                });
-            };
-
-            var stepRunScripts = function() {
-                var group = this.group();
-                //Ao extender, nao executa o script no filho, apenas no pai
-                if (view.extend) {
-                    return;
-                }
-
-                view.$('script').each(function() {
-                    var script = this;
-                    var done = group();
-
-                    //
-                    if (script.attr('cheerio-template') === undefined) {
-                        return done();
-                    }
-                    script.removeAttr('cheerio-template');
-                    var src = script.attr('src');
-
-                    // htmls runs script content only when src is not set
-                    if (!src && script.html().trim() !== '') {
-                        view.sandbox.__actualScript = script;
-                        view.sandbox.run(script.html());
-                    }
-
-                    script.remove();
-                    return done();
-                });
-            };
-
-            var stepRenderPage = function(err) {
-                if (err) {
-                    console.error(err); // errors are not fatal
-                }
-
-                //Renderiza a pagina
-                if (view.extend) {
-                    options.__child = view;
-                    //Chama o template pai, que este extende
-                    CheerioTemplate.engine(view.extend, options, function(err, html) {
-                        view.sandbox.dispose();
-                        callback(null, html);
                     });
-                } else {
-                    //Nao extende nada, renderiza
-                    view.sandbox.dispose();
-                    callback(null, view.$.html());
-                }
-            };
+                };
 
-            Step(
-                    stepParseExtends,
-                    stepParseBlocks,
-                    stepParseIncludes,
-                    stepParseMixins,
-                    stepRunScripts,
-                    stepRenderPage
-                    );
-        });
+                /**
+                 * Run all scripts tag
+                 */
+                var stepRunScripts = function() {
+                    var group = this.group();
+
+                    //Skip, if it extends other
+                    //childs content block was inserted in parent code after this step
+                    if (view.extend) {
+                        return;
+                    }
+
+                    view.$('script').each(function() {
+                        var done = group();
+
+                        //
+                        if (this.attr('cheerio-template') === undefined) {
+                            return done();
+                        }
+
+                        var src = this.attr('src');
+                        // htmls runs script content only when src is not set
+                        if (!src && this.html().trim() !== '') {
+                            view.sandbox.__actualScript = this;
+                            view.sandbox.run(this.html());
+                        }
+
+                        this.remove();
+                        return done();
+                    });
+                };
+
+                /**
+                 * Finaly, render html page
+                 */
+                var stepRenderPage = function() {
+                    if (view.extend) {
+                        options.__child = view;
+                        //Call parent view
+                        CheerioTemplate.engine(view.extend, options, function(err, html) {
+                            view.sandbox.dispose();
+                            callback(null, html);
+                        });
+                    } else {
+                        //no parent's, render this script
+                        view.sandbox.dispose();
+                        callback(null, view.$.html());
+                    }
+                };
+
+                //magic
+                Step(
+                        stepParseExtends,
+                        stepParseBlocks,
+                        stepParseIncludes,
+                        stepParseMixins,
+                        stepRunScripts,
+                        stepRenderPage
+                        );
+            });
+        }
     };
 
     return CheerioTemplate;
